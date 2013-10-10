dd5216e599d9099d936d87c73ed849cb03132ce5
Fergus McDowall <fergusmcdowall@gmail.com>
2013-10-10 13:27:36 +0200

    Update vmsetup.sh

--- vmsetup.sh
+++ vmsetup.sh
@@ -4,8 +4,5 @@ sudo apt-get -y install python-software-properties
 sudo add-apt-repository -y ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get -y install curl git nodejs make g++
-git clone https://github.com/fergiemcdowall/forage.git
-sudo chown -R vagrant:vagrant forage
-cd /home/vagrant/forage
-npm install
-node forage
+npm install -g forage
+forage
